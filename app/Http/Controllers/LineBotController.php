4c237afb3899e6cc7ef5b7bbf6984eb1181cc379
yumauma20 <71yuma10@gmail.com>
2020-03-13 17:02:32 +0900

    ログ機能の追加

--- app/Http/Controllers/LineBotController.php
+++ app/Http/Controllers/LineBotController.php
@@ -4,10 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Log;
+
 class LineBotController extends Controller
 {
     public function index()
     {
         return view('linebot.index');
+    }
+
+    public function parrot(Request $request)
+    {
+        Log::debug($request->header());
+        Log::debug($request->input());
     }
 }

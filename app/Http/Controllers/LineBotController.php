a58cd819a156c6b5942baf3be36bcee66ac6b635
yumauma20 <71yuma10@gmail.com>
2020-03-13 17:28:48 +0900

    署名の検証

--- app/Http/Controllers/LineBotController.php
+++ app/Http/Controllers/LineBotController.php
@@ -3,9 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use Illuminate\Support\Facades\Log;
-
+use LINE\LINEBot;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 class LineBotController extends Controller
 {
     public function index()
@@ -17,5 +17,13 @@ class LineBotController extends Controller
     {
         Log::debug($request->header());
         Log::debug($request->input());
+
+        $httpClient = new CurlHTTPClient(env('LINE_ACCESS_TOKEN'));
+        $lineBot = new LINEBot($httpClient, ['channelSecret' => env('LINE_CHANNEL_SECRET')]);
+
+        $signature = $request->header('x-line-signature');
+        if (!$lineBot->validateSignature($request->getContent(), $signature)) {
+            abort(400, 'Invalid signature');
+        }
     }
 }

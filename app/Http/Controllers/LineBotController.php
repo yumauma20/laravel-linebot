398bc9358fd391674553dfe7f39428aaaebaf377
yumauma20 <71yuma10@gmail.com>
2020-03-14 16:06:17 +0900

    飲食店検索結果返信機能の作成

--- app/Http/Controllers/LineBotController.php
+++ app/Http/Controllers/LineBotController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\Gurunavi;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use LINE\LINEBot;
 use LINE\LINEBot\Event\MessageEvent\TextMessage;
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+
 class LineBotController extends Controller
 {
     public function index()
@@ -14,7 +16,7 @@ class LineBotController extends Controller
         return view('linebot.index');
     }
 
-    public function parrot(Request $request)
+    public function restaurants(Request $request)
     {
         Log::debug($request->header());
         Log::debug($request->input());
@@ -37,8 +39,25 @@ class LineBotController extends Controller
                 continue;
             }
 
+            $gurunavi = new Gurunavi();
+            $gurunaviResponse = $gurunavi->searchRestaurants($event->getText());
+
+            if (array_key_exists('error', $gurunaviResponse)) {
+                $replyText = $gurunaviResponse['error'][0]['message'];
+                $replyToken = $event->getReplyTolen();
+                $lineBot->replyText($replyToken, $replyText);
+                continue;
+            }
+
+            $replyText = '';
+            foreach($gurunaviResponse['rest'] as $restaurant) {
+                $replyText .=
+                    $restaurant['name'] . "\n" .
+                    $restaurant['url'] . "\n" .
+                    "\n";
+            }
+
             $replyToken = $event->getReplyToken();
-            $replyText = $event->getText();
             $lineBot->replyText($replyToken, $replyText);
         }
     }
